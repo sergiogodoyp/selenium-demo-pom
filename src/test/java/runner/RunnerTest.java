fa539a3efd3683392a88294801f12af43d32ac87
Sergio Godoy <sergio_jgp@hotmail.com>
2024-12-29 01:32:44 -0500

    mantenimiento de escenarios

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -1,7 +1,6 @@
 package runner;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
@@ -15,6 +14,7 @@ import org.junit.runner.RunWith;
         tags = "@Prueba"
 
 )
+
 
 public class RunnerTest {
 }
